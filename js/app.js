273e403455c2a3e3cebc5e58eacaf1726407d8ee
DNI9 <itsr9boi@gmail.com>
2020-05-25 15:42:00 +0530

    appear scrollToTop with js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 // wow.js
 new WOW().init();
+
+// add scroll to top button after scroll
+let scrollToTop = document.querySelector(".scrollToTop");
+window.onscroll = (e) => {
+  if (window.pageYOffset > 130) {
+    scrollToTop.classList.add("show");
+  } else {
+    scrollToTop.classList.remove("show");
+  }
+};
